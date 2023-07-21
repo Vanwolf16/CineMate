bf04c32f8b87450f493129ee108c47d2a558fa60
vanwolf16 <d.murillo28@yahoo.com>
2023-07-21 14:29:26 -0700

    Fetch API with Updating the Card

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,18 +1,30 @@
 import { Link } from "react-router-dom";
 import Card from "../components/Card";
+import { useEffect, useState } from "react";
 
 export default function MovieList() {
+
+  const [movies,setMovies] = useState([]);
+
+  useEffect( () => {
+    async function fetchMovies(){
+      const response = await fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=089969fe07c408b75ad164158596950e");
+      const data = await response.json();
+      setMovies(data.results)
+    }
+    fetchMovies();
+  }, [])
+
   return (
     <main>
          <section className="max-w-7xl mx-auto py-7">
           <div className="flex justify-start flex-wrap ">
 
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+            {movies.map( (movie) => (
+              <Card key={movie.id} movie={movie}/>
+            ))}
+
+            
 
           </div>
          </section>
